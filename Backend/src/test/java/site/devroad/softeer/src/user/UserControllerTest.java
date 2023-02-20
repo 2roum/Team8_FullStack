ef8daa8cfb51bcda3beae15ed6de72886397ef16
Toa <ld980105@snu.ac.kr>
2023-02-20 15:10:01 +0900

    feat : 로드맵을 시작하지 않은 유저 정보 받아오기

--- Backend/src/test/java/site/devroad/softeer/src/user/UserControllerTest.java
+++ Backend/src/test/java/site/devroad/softeer/src/user/UserControllerTest.java
@@ -31,7 +31,7 @@ class UserControllerTest {
 
     @Test
     @DisplayName("회원가입 테스트")
-    void 회원가입_테스트() throws Exception {
+    void signupTest() throws Exception {
         //given
         given(userService.join(any(PostSignUpReq.class))).willReturn(
                 new PostSignUpRes(1000L)
@@ -57,7 +57,7 @@ class UserControllerTest {
 
     @Test
     @DisplayName("로그인 테스트")
-    void 로그인_테스트() throws Exception {
+    void signinTest() throws Exception {
         //given
         given(userService.signIn(any(PostSignInReq.class))).willReturn(
                 new PostSignInRes("test-jwt", false)
@@ -81,9 +81,31 @@ class UserControllerTest {
 
     @Test
     @DisplayName("로드맵이 없는 유저 정보 받아오기")
-    void 로드맵이없는_유저_정보_받아오기_테스트() throws Exception {
+    void getUserDetailNoRoadmap() throws Exception {
         //given
         GetUserDetailRes testDto = GetUserDetailRes.createNoRoadmapUserDetail(1000L, "testName", false);
+        given(userService.getUserDetail(any(Long.class))).willReturn(testDto);
+        given(jwtUtility.getAccountId("testJwt")).willReturn(1000L);
+        String expectedJson = new ObjectMapper().writeValueAsString(testDto);
+
+        //when
+        mockMvc.perform(
+                        get("/api/user")
+                                .header("jwt", "testJwt")
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJson))
+                .andDo(print());
+
+        // verify : 해당 객체의 메소드가 실행 여부를 체크
+        verify(userService).getUserDetail(any(Long.class));
+    }
+
+    @Test
+    @DisplayName("로드맵을 시작하지 않은 유저 정보 받아오기")
+    void getUserDetailNotStartedRoadmap () throws Exception {
+        //given
+        GetUserDetailRes testDto = GetUserDetailRes.createNotStartUserDetail(1000L, 10L, 2L, "test", false);
         given(userService.getUserDetail(any(Long.class))).willReturn(testDto);
         given(jwtUtility.getAccountId("testJwt")).willReturn(1000L);
         String expectedJson = new ObjectMapper().writeValueAsString(testDto);
