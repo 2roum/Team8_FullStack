ddfbb76f0d936278049f7171a416f4620c1af09e
Toa <ld980105@snu.ac.kr>
2023-02-20 14:37:33 +0900

    feat : 로그인 컨트롤러 테스트 추가

--- Backend/src/test/java/site/devroad/softeer/src/user/UserControllerTest.java
+++ Backend/src/test/java/site/devroad/softeer/src/user/UserControllerTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import site.devroad.softeer.src.user.dto.PostSignInReq;
+import site.devroad.softeer.src.user.dto.PostSignInRes;
 import site.devroad.softeer.src.user.dto.PostSignUpReq;
 import site.devroad.softeer.src.user.dto.PostSignUpRes;
 import site.devroad.softeer.utility.JwtUtility;
@@ -53,5 +55,29 @@ class UserControllerTest {
 
         // verify : 해당 객체의 메소드가 실행 여부를 체크
         verify(userService).join(any(PostSignUpReq.class));
+    }
+
+    @Test
+    @DisplayName("로그인 테스트")
+    void 로그인_테스트() throws Exception {
+        //given
+        given(userService.signIn(any(PostSignInReq.class))).willReturn(
+                new PostSignInRes("test-jwt", false)
+        );
+        //when
+        mockMvc.perform(
+                        post("/api/user/signin")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\"email\":\"signupTest@gmail.com\"," +
+                                        "\"password\":\"1234\"}")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").exists())
+                .andExpect(jsonPath("$.jwt").exists())
+                .andExpect(jsonPath("$.admin").exists())
+                .andDo(print());
+
+        // verify : 해당 객체의 메소드가 실행 여부를 체크
+        verify(userService).signIn(any(PostSignInReq.class));
     }
 }
