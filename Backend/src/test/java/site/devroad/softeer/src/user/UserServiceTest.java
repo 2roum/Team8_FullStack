5e94dc2706f34332c1c038253f59403831a0f0a0
Toa <ld980105@snu.ac.kr>
2023-02-20 17:31:39 +0900

    feat : 로그인이 실패하는 경우 구현

--- Backend/src/test/java/site/devroad/softeer/src/user/UserServiceTest.java
+++ Backend/src/test/java/site/devroad/softeer/src/user/UserServiceTest.java
@@ -2,121 +2,130 @@ package site.devroad.softeer.src.user;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import site.devroad.softeer.exceptions.CustomException;
 import site.devroad.softeer.exceptions.ExceptionType;
+import site.devroad.softeer.src.exam.ExamSubmissionRepo;
+import site.devroad.softeer.src.roadmap.RoadmapRepo;
+import site.devroad.softeer.src.roadmap.chapter.ChapterRepo;
+import site.devroad.softeer.src.roadmap.completedchapter.CompletedChapterRepo;
+import site.devroad.softeer.src.roadmap.subject.SubjectRepo;
 import site.devroad.softeer.src.user.dto.PostSignInReq;
+import site.devroad.softeer.src.user.dto.PostSignInRes;
 import site.devroad.softeer.src.user.dto.PostSignUpReq;
+import site.devroad.softeer.src.user.dto.PostSignUpRes;
 import site.devroad.softeer.src.user.model.Account;
 import site.devroad.softeer.src.user.model.LoginInfo;
 import site.devroad.softeer.utility.JwtUtility;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@SpringBootTest
-@Transactional
+@SpringBootTest(classes = {UserService.class})
 class UserServiceTest {
-    private static final Logger logger = LoggerFactory.getLogger(UserServiceTest.class);
+    @MockBean
+    UserRepo userRepo;
+
+    @MockBean
+    RoadmapRepo roadmapRepo;
+
+    @MockBean
+    SubjectRepo subjectRepo;
+
+    @MockBean
+    ExamSubmissionRepo submissionRepo;
+
+    @MockBean
+    CompletedChapterRepo completedChapterRepo;
+
+    @MockBean
+    ChapterRepo chapterRepo;
+
+    @MockBean
+    JwtUtility jwtUtility;
+
     @Autowired
     UserService userService;
 
-    @Autowired
-    UserRepo userRepo;
-
-    @Autowired
-    JwtUtility jwtUtility;
-
     @Test
-    void signupSuccessTest() {
-        String email = "successTest@naver.com";
-        String phone = "1234";
-        try {
-            PostSignUpReq postSignUpReq = new PostSignUpReq(email, "test1", phone, "1234");
-            userService.join(postSignUpReq);
-        } catch (CustomException e) {
-            e.printStackTrace();
-        }
-        Optional<LoginInfo> optionalLoginInfo = userRepo.findLoginInfoByEmail(email);
-        Optional<Account> optionalAccount = userRepo.findByPhone(phone);
-        assertThat(optionalLoginInfo).isPresent();
-        assertThat(optionalAccount).isPresent();
-    }
-
-    @Test
-    void duplicateEmailTest() {
+    @DisplayName("회원 가입 서비스 메서드 테스트")
+    void join() {
         //given
-        String dupEmail = "dupdupEmail@naver.com";
-        String phone = "01040402020";
-        PostSignUpReq postSignUpReq1 = new PostSignUpReq(dupEmail, "test1", phone, "1234");
-        PostSignUpReq postSignUpReq2 = new PostSignUpReq(dupEmail, "test1", "01020202020", "1234");
-        //when
-        try {
-            userService.join(postSignUpReq1);
-            userService.join(postSignUpReq2);
-        }
-        //then
-        catch (CustomException e) {
-            assertThat(e.getExceptionType()).isEqualTo(ExceptionType.POST_ACCOUNT_EMAIL_DUPLICATED);
-        }
-    }
+        String name = "test";
+        String type = "Student";
+        String phone = "01042427272";
+        String email = "test@naver.com";
+        String password = "1234";
+        Long roadmapId = 12L;
+        Long accountId = 1000L;
+        Long loginInfoId = 50L;
 
-    @Test
-    void duplicatePhoneTest() {
-        //given
-        String dupPhone = "01000000000";
-        String email = "test@gmail.com";
-
-        PostSignUpReq postSignUpReq1 = new PostSignUpReq(email, "test1", dupPhone, "1234");
-        PostSignUpReq postSignUpReq2 = new PostSignUpReq("hello@hanmail.net", "test2", dupPhone, "1234");
+        PostSignUpReq postSignUpReq = new PostSignUpReq(email, name, phone, password);
 
         //when
-        try {
-            userService.join(postSignUpReq1);
-            userService.join(postSignUpReq2);
-        }
+        Mockito.when(userRepo.findByPhone(phone)).thenReturn(Optional.empty());
+        Mockito.when(userRepo.findLoginInfoByEmail(email)).thenReturn(Optional.empty());
+        Mockito.when(userRepo.createAccountInfo(name, phone, type))
+                .thenReturn(new Account(accountId, name, roadmapId, phone, type, null, null));
+        Mockito.when(userRepo.createLoginInfo(email, password, accountId))
+                .thenReturn(new LoginInfo(loginInfoId, email, password, accountId));
+        PostSignUpRes joinResDto = userService.join(postSignUpReq);
+
         //then
-        catch (CustomException e) {
-            assertThat(e.getExceptionType()).isEqualTo(ExceptionType.POST_ACCOUNT_PHONE_DUPLICATED);
-        }
+        assertThat(joinResDto.getUserId()).isEqualTo(accountId);
     }
 
     @Test
-    void signInTest() {
-        //TODO : 회원가입 로직 및 회원 탈퇴 로직을 구현해서 테스트
-        PostSignInReq postSignInReq = new PostSignInReq("1234@naver.com", "1234");
-        try {
-            Long userId = userService.signIn(postSignInReq);
-            assertThat(userId).isInstanceOf(Long.class);
-        } catch (CustomException e) {
-            logger.warn(e.getMessage());
-        }
+    @DisplayName("아이디에 해당하는 계정이 없어 로그인이 실패하는 경우")
+    void signInFail() {
+        //given
+        PostSignInReq postSignInReq = new PostSignInReq("test@naver.com", "1234");
+
+        //when
+        Mockito.when(userRepo.findLoginInfoByEmail(postSignInReq.getEmail()))
+                .thenReturn(Optional.empty());
+
+        //then
+        assertThatThrownBy(() -> userService.signIn(postSignInReq))
+                .isInstanceOf(CustomException.class);
     }
 
     @Test
-    @DisplayName("사용자가 Exam을 올바르게 구매할 수 있는지 확인.")
-    void examSubmissionTest(){
-        try {
-            //given
-            Account account = userRepo.createAccountInfo("hi", "0100000092", "Student");
-            userRepo.doSubscribe(account.getId());
+    @DisplayName("로그인이 성공하는 경우")
+    void signInSuccess() {
 
-            //when
-            Boolean result = userRepo.isUserSubscribed(account.getId());
+    }
 
-            //then
-            assertTrue(result);
-        }catch (CustomException e){
-            logger.warn(e.getMessage());
-            e.printStackTrace();
-            throw new RuntimeException();
-        }
+    @Test
+    void validateSignUp() {
+    }
+
+    @Test
+    void isAdmin() {
+    }
+
+    @Test
+    void getNoRoadmapUsers() {
+    }
+
+    @Test
+    void isUserSubscribe() {
+    }
+
+    @Test
+    void getUserDetail() {
+    }
+
+    @Test
+    void getAllUser() {
+    }
+
+    @Test
+    void getAccountById() {
     }
 }
