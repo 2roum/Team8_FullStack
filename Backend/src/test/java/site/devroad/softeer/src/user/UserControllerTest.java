14f8b5f902d758b56142a316902a3a49cacd3574
Toa <ld980105@snu.ac.kr>
2023-02-20 15:05:19 +0900

    feat : 로드맵이 없는 유저 정보 받아오기

--- Backend/src/test/java/site/devroad/softeer/src/user/UserControllerTest.java
+++ Backend/src/test/java/site/devroad/softeer/src/user/UserControllerTest.java
@@ -1,5 +1,6 @@
 package site.devroad.softeer.src.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,19 +8,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import site.devroad.softeer.src.user.dto.PostSignInReq;
-import site.devroad.softeer.src.user.dto.PostSignInRes;
-import site.devroad.softeer.src.user.dto.PostSignUpReq;
-import site.devroad.softeer.src.user.dto.PostSignUpRes;
+import site.devroad.softeer.src.user.dto.*;
 import site.devroad.softeer.utility.JwtUtility;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @WebMvcTest(UserController.class)
@@ -79,5 +77,27 @@ class UserControllerTest {
 
         // verify : 해당 객체의 메소드가 실행 여부를 체크
         verify(userService).signIn(any(PostSignInReq.class));
+    }
+
+    @Test
+    @DisplayName("로드맵이 없는 유저 정보 받아오기")
+    void 로드맵이없는_유저_정보_받아오기_테스트() throws Exception {
+        //given
+        GetUserDetailRes testDto = GetUserDetailRes.createNoRoadmapUserDetail(1000L, "testName", false);
+        given(userService.getUserDetail(any(Long.class))).willReturn(testDto);
+        given(jwtUtility.getAccountId("testJwt")).willReturn(1000L);
+        String expectedJson = new ObjectMapper().writeValueAsString(testDto);
+
+        //when
+        mockMvc.perform(
+                        get("/api/user")
+                                .header("jwt", "testJwt")
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJson))
+                .andDo(print());
+
+        // verify : 해당 객체의 메소드가 실행 여부를 체크
+        verify(userService).getUserDetail(any(Long.class));
     }
 }
